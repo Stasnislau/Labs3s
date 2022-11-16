dcfe99fa817588b0d48b631804134cafc629a4e8
Stanislau Ryzhkou <stanislaurv@gmail.com>
2022-11-16 10:24:21 +0100

    refactoring

--- lab1.hpp
+++ lab1.hpp
@@ -34,11 +34,15 @@ public:
         Node &operator*() const;
         Node *operator->();
     };
+    // returns iterator to the first element
     Iterator begin();
+    // returns iterator to the last element
     Iterator end();
-
+    // empty constructor
     Sequence();
+    // copy constructor
     Sequence(const Sequence<t_key, t_info> &other);
+    // returns the number of elements in the sequence
     int getLength() const;
     // add a new element at the end or head of the sequence depending on the value of toHead, default is to the end
     bool add(t_key key, t_info info, bool toHead = false);
@@ -46,15 +50,22 @@ public:
     bool insert(t_key key, t_info info, const t_key prevKey, int occurrence = 0);
     // removes the specified occurrence of the key
     bool remove(t_key key, int occ = 0);
+    // returns true if the sequence is empty
     bool isEmpty() const;
+    // overload of = operator
     Sequence &operator=(const Sequence &other);
     // swaps the elements at the specified occurrences of the keys
     bool moveNodes(t_key key1, t_key key2, int occurrences1 = 0, int occurrences2 = 0);
+    // returns the number of occurrences of a specified key
     int occurrences(t_key key) const;
+    // returns the position of the specified occurrence of the key in the list
     int positionOf(t_key key, int occurrence = 0) const;
+    // checks if an element with the specified key exists in the sequence
     bool contains(t_key key) const;
     // removes all elements from the sequence
     void clear();
+
+    // overload of << operator
     friend std::ostream &operator<<(std::ostream &out, Sequence<t_key, t_info> &other)
     {
          try
